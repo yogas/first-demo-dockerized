3f8d653ffd6627a7406a02b26516a50ce08a21f0
Vladimir Shapovalov <vladimir.shapovalov@nimbusweb.co>
2021-08-21 21:43:38 +0700

    init

---
+++ helpers.mjs
@@ -0,0 +1,9 @@
+const connectToDatabase = async () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve()
+    }, 1000)
+  })
+}
+
+export default connectToDatabase
